4cb5df5134c31dbb08d8a8e1314d93cb9cd878c7
/ Anup Sarkar <aronno1107@gmail.com>
2023-11-16 14:21:59 +0800

    Update ClassToRecordConverter.cs

--- CsharpRecordToClass/ClassToRecordConverter.cs
+++ CsharpRecordToClass/ClassToRecordConverter.cs
@@ -17,46 +17,45 @@ string classDeclaration = """
 
 """;
 
-        string classNamePattern = @"\s+class\s+(\w+)";
-        string propertyPattern = @"\s+(public|private|protected|internal)\s+(\w+\??)\s+(\w+)\s*{\s*get;\s*set;\s*}";
 
-        // Extract class name
-        Match classNameMatch = Regex.Match(classDeclaration, classNamePattern);
-        if (classNameMatch.Success && classNameMatch.Groups.Count == 2)
+string classNamePattern = @"\s+class\s+(\w+)";
+string propertyPattern = @"\s+(public|private|protected|internal)\s+([\w<>,\s]+\??)\s+(\w+)\s*{\s*get;\s*set;\s*}";
+
+// Extract class name
+Match classNameMatch = Regex.Match(classDeclaration, classNamePattern);
+if (classNameMatch.Success && classNameMatch.Groups.Count == 2)
+{
+    string className = classNameMatch.Groups[1].Value;
+
+    StringBuilder recordBuilder = new StringBuilder();
+    recordBuilder.AppendLine($"public record {className}");
+
+    // Extract properties
+    MatchCollection propertyMatches = Regex.Matches(classDeclaration, propertyPattern);
+    if (propertyMatches.Count > 0)
+    {
+        recordBuilder.AppendLine("(");
+
+        foreach (Match match in propertyMatches)
         {
-            string className = classNameMatch.Groups[1].Value;
-
-            // Generate record representation
-            StringBuilder recordBuilder = new StringBuilder();
-            recordBuilder.AppendLine($"public record {className}");
-
-            // Extract properties
-            MatchCollection matches = Regex.Matches(classDeclaration, propertyPattern);
-            if (matches.Count > 0)
+            if (match.Groups.Count == 4)
             {
-                recordBuilder.AppendLine("(");
+                string type = match.Groups[2].Value;
+                string name = match.Groups[3].Value;
 
-                foreach (Match match in matches)
-                {
-                    if (match.Groups.Count == 4)
-                    {
-                        string type = match.Groups[2].Value;
-                        string name = match.Groups[3].Value;
-
-                        // Generate property
-                        recordBuilder.AppendLine($"    {type} {name},");
-                    }
-                }
-
-                // Remove the trailing comma from the last property
-                recordBuilder.Remove(recordBuilder.Length - 3, 1);
-                recordBuilder.AppendLine(");");
+                recordBuilder.AppendLine($"    {type} {name},");
             }
-            else
-            {
-                // If there are no properties, just close the record declaration
-                recordBuilder.AppendLine(";");
-            }
-
-            Console.WriteLine(recordBuilder.ToString());
         }
+
+        // Remove the trailing comma from the last property
+        recordBuilder.Remove(recordBuilder.Length - 3, 1);
+        recordBuilder.AppendLine(");");
+    }
+    else
+    {
+        recordBuilder.AppendLine(";");
+    }
+
+    Console.WriteLine(recordBuilder.ToString());
+}
+ 
